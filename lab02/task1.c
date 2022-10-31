f9e9fc9f47380b02b4243cfd73b019a93042a4c5
qAuroraq <20281310@bjtu.edu.cn>
2022-10-31 20:46:12 +0800

    finish the program

--- lab02/task1.c
+++ lab02/task1.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct{
     int row;
     int column;
-}paramters;
+}parameters;
 
-void *check_Row(void *param)
-void *check_Col(void *param)
-void *check_Nine(void *param)
+int g[9][9] = {{0, 1, 2, 3, 4, 5, 6, 7, 8},
+               {1, 2, 3, 4, 5, 6, 7, 8, 0},
+               {2, 3, 4, 5, 6, 7, 8, 9, 1},
+               {3, 4, 5, 6, 7, 8, 0, 1, 2},
+               {4, 5, 6, 7, 8, 0, 1, 2, 3},
+               {5, 6, 7, 8, 0, 1, 2, 3, 4},
+               {6, 7, 8, 0, 1, 2, 3, 4, 5},
+               {7, 8, 0, 1, 2, 3, 4, 5, 6},
+               {8, 0, 1, 2, 3, 4, 5, 6, 7},
+              };
+
+void *check_Row(void *param);
+void *check_Col(void *param);
+void *check_Nine(void *param);
 
 void *check_Row(void *params)
 {
@@ -48,7 +60,7 @@ void *check_Nine(void *params)
     int row = p->row;
     int col = p->column;
     for(int i = row; i < row + 3; i ++ ){
-        for(int j = col; j < col + 3; j ++ {
+        for(int j = col; j < col + 3; j ++) {
             vis[g[i][j]] ++;
             if(vis[g[i][j]] > 1)    return (void *)0;
         }
@@ -60,18 +72,21 @@ int main()
 {
     parameters *data = (parameters *)malloc(sizeof(parameters));
     data->row = 0;
-    data->col = 0;
+    data->column = 0;
     void *res[11];
     pthread_t t_Row,t_Col;
     //pthread_t Nine_1, Nine_2, Nine_3, Nine_4, Nine_5, Nine_6, Nine_7, Nine_8, Nine_9;
-    pthread_t Nine[9]
+    pthread_t Nine[9];
     pthread_create(&t_Row, NULL, check_Row, (void *)data);
     pthread_create(&t_Col, NULL ,check_Col, (void *)data);
+    parameters *data1 = (parameters *)malloc(sizeof(parameters) * 9);
+    int k = 0;
     for(int i = 0; i < 9; i += 3){
         for(int j = 0; j < 9; j += 3){
-            data->col = j;
-            data->row = i;
-            phtread_create(&t_Nine[i], NULL, check_Nine, (void *)data);
+            (data1 + k)->column = j;
+            (data1 + k)->row = i;
+            pthread_create(&Nine[i], NULL, check_Nine, (void *)(data1 + k));
+            k ++;
         }
     }
     /*
@@ -101,8 +116,8 @@ int main()
         pthread_join(t_Col, &res[1]);
         if((int *)res[1] == 0)  flag = 0;
         if(flag){
-            for(int i = 0; i < 9; i ++ ){
-                pthread_join(t_Nine[i], &res[i]);
+            for(int i = 2; i < 11; i ++ ){
+                pthread_join(Nine[i], &res[i]);
                 if((int *)res[i] == 0) {
                     flag = 0;
                     break;
@@ -110,8 +125,8 @@ int main()
             }
         }
     }
-    if(flag)    printf("failed");
-    else    printf("success");
+    if(flag)    puts("success");
+    else    puts("fail");
 }
 
 
--- lab02/task2.c
+++ lab02/task2.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct{
     int st;
     int ed;
 }parameters;
 
-int array[10] = {0,1,2,3,4,5,6,7,8,9};
+int array[11] = {0,1,2,3,5,6,4,7,8,9};
 
 void* my_sort(void *data)
 {
-    int st = data->st;
-    int ed = data->ed;
-    for(int i = st; i < ed; i ++ )
+    parameters *p = (parameters *)data;
+    int st = p->st;
+    int ed = p->ed;
+    for(int i = st; i < ed - 1; i ++ )
     {
-        for(int j = st + 1; j < ed; j ++ )
+        for(int j = st; j + 1 < ed; j ++ )
         {
-            if(array[i] > array[j]) {
-                int tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
+            if(array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
             }
         }
     }
+    for(int i = st; i < ed; i ++ )  printf("%d ",array[i]);
+    puts("");
 }
 
 void* merge_array(void *data)
 {
+    parameters *pdata = (parameters *) data;
+    int st = pdata->st, ed = pdata->ed;
     int len = ed - st;
     int mid = len / 2;
     int i = st, j = mid;
@@ -53,26 +60,34 @@ void* merge_array(void *data)
         j ++;
         p ++;
     }
+    p = p - len;
+    for(int i = 0; i < len; i ++ ){
+        array[i] = *p;
+        p ++;
+    }
 }
     
 int main()
 {
     parameters* data = (parameters *)malloc(sizeof (parameters));
     data->st = 0;
-    data->ed = 10 / 2;
+    data->ed = 10;
+    parameters *data1 = (parameters *)malloc(sizeof (parameters));
+    data1->st = 0;
+    data1->ed = 5;
+    parameters *data2 = (parameters *)malloc(sizeof (parameters));
+    data2->st = 5;
+    data2->ed = 10;
     pthread_t sort1,sort2,merge;
-    pthread_create(&sort1, NULL, my_sort, (void *)data);
-    data->st = data->ed;
-    data->ed = 10;
-    pthread_create(&sort2, NULL, my_sort, (void *)data);
-    data->st = 0;
-    data->ed = 10;
+    pthread_create(&sort1, NULL, my_sort, (void *)data1);
+    pthread_create(&sort2, NULL, my_sort, (void *)data2);
+    pthread_join(sort1, NULL);
+    pthread_join(sort2, NULL);
     pthread_create(&merge, NULL, merge_array, (void *)data);
-    pthread_join(sort1);
-    pthread_join(sort2);
-    pthread_join(merge);
+    pthread_join(merge, NULL);
     for(int i = 0; i < 10; i ++ ){
         printf("%d ", array[i]);
     }
+    puts("");
     return 0;
 }
